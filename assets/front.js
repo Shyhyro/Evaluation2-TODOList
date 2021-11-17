c43fb3c32957064d85a7095d5361bd15ece595b8
Shyhyro <Bosquet.laurie@outlook.com>
2021-11-17 17:33:35 +0100

    update

--- assets/front.js
+++ assets/front.js
@@ -1,34 +1,93 @@
 
-import {SayObject} from "./js/SayObject";
+//import {SayObject} from "./js/SayObject";
+//SayObject.addItem();
+//SayObject.sayGoodbye();
+
 import "./css/front.css";
 
 let addingItem = document.querySelector('#addItem button');
-
 let table = document.getElementById('itemsList');
+let clear = document.querySelector('#clearItem button');
 
-if (localStorage.getItem('Item') !== null) {
-    let items = localStorage.getItem('Item');
 
-    let tr = document.createElement('tr');
-    table.appendChild(tr);
-    tr.innerHTML = "<td>"+ items +"</td>" +
-        "<td class='action'><i class=\"far fa-check-circle\"></i><i class=\"fas fa-edit\"></i><i class=\"far fa-times-circle\"></i></td>";
+// Clear list item & localstorage
+function clearItem() {
+    localStorage.clear();
+    table.innerHTML="";
 }
 
-addingItem.addEventListener('click', function () {
+clear.addEventListener('click', clearItem);
 
+// Search and add local storage value
+function localActut(){
+    table.innerHTML="";
+
+    for(let i=0, len=localStorage.length; i<len; i++) {
+        let key = localStorage.key(i);
+        let value = localStorage[key];
+        let tr = document.createElement('tr');
+        table.appendChild(tr);
+        tr.innerHTML = "<td>"+ value +"</td>" +
+            "<td class='action'>" +
+            "<i class=\"far fa-check-circle\" data-id='" + key + "'></i>" +
+            "<i class=\"fas fa-edit\" data-id='" + key + "'></i>" +
+            "<i class=\"far fa-times-circle\" data-id='" + key + "'></i>" +
+            "</td>";
+    }
+
+    checkItem();
+    deleteItem();
+}
+
+// Add a new value
+addingItem.addEventListener('click', function () {
     let newItem = document.getElementById('newItem');
 
-    console.log(newItem.value);
+    if (newItem.value.length > 0) {
+        let number = localStorage.length;
+        let id = "Item" + number;
+        console.log("Add: "+ id);
+        let tr = document.createElement('tr');
+        table.appendChild(tr);
+        tr.innerHTML = "<td>"+ newItem.value +"</td>" +
+            "<td class='action'>" +
+            "<i class=\"far fa-check-circle\" data-id='" + id + "'></i>" +
+            "<i class=\"fas fa-edit\" data-id='" + id + "'></i>" +
+            "<i class=\"far fa-times-circle\" data-id='" + id + "'></i>" +
+            "</td>";
+        localStorage.setItem(id, newItem.value);
 
-    let tr = document.createElement('tr');
-    table.appendChild(tr);
-    tr.innerHTML = "<td>"+ newItem.value +"</td>" +
-        "<td class='action'><i class=\"far fa-check-circle\"></i><i class=\"fas fa-edit\"></i><i class=\"far fa-times-circle\"></i></td>";
-
-    localStorage.setItem('Item', newItem.value);
-
+        setTimeout(localActut, 800);
+    }
 });
 
-//SayObject.addItem();
-//SayObject.sayGoodbye();
+// Check a item
+function checkItem() {
+    let check = document.querySelectorAll(".fa-check-circle");
+
+    check.forEach(function (e) {
+        e.addEventListener('click', function () {
+            let id = e.dataset.id;
+            e.style.color = "#95D6B7";
+            console.log("Check: " + id);
+        })
+    })
+}
+
+// Delete a item
+function deleteItem() {
+    let check = document.querySelectorAll(".fa-times-circle");
+
+    check.forEach(function (e) {
+        e.addEventListener('click', function () {
+            let id = e.dataset.id;
+            console.log("Delete: " + id);
+            localStorage.removeItem(id);
+
+            setTimeout(localActut, 800);
+        })
+    })
+}
+
+// Call
+localActut();
